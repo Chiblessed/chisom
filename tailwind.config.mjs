4ade012d8611ecc30c2823724f64be79fedf869e
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2025-01-06 04:03:35 +0100

    done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'fontPrim': ['Jost'],
-        'fontPrim1': ['Rye'],
-
-            },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
