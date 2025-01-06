aeb8dc4d29758a95ac4acd95423e3454938e5463
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2025-01-06 11:15:37 +0100

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,12 @@
-"use client"
-import { useState, useEffect } from "react";
+
 import Hero from "@/app/home/page";
 import Index from "@/components/preloader";
-import { AnimatePresence } from "framer-motion";
+
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-setTimeout(() => {
-  setIsLoading(false);
-  document.body.style.cursor = 'default'
-  window.scrollTo(0,0);
-}, 2000)
-  }, [])
+  
   return (
 <div>
-<AnimatePresence mode='wait'>
-        {isLoading && <Index />}
-      </AnimatePresence>
-  
+
   <Hero />
     </div>
   );
