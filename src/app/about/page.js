96d43dedbad03e23492fe2a2f64d1add327d5db1
Chiblessed <chizzyhanz56@gmail.com>
2025-07-26 02:12:44 -1100

    feat:Minor Changes - Fixed the scroll bug - Added a new project - Made it mobile responsive

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,34 +1,64 @@
+"use client";
 
-
+import { useEffect } from "react";
+import SplitType from "split-type";
+import gsap from "gsap";
 
 const About = () => {
-    return ( <>
-<main className="bg-primary -z-10  h-full">
-      <section className="">
-        <h2 className={`font-oswald text-center text-6xl text-black `}>
+  useEffect(() => {
+    const splitText = new SplitType(".aboutmain", { types: "words, chars" });
+
+    gsap.fromTo(
+      ".aboutmain .word",
+      {
+        y: 20,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.08,
+        duration: 0.4,
+        ease: "easeInOut",
+      }
+    );
+
+    return () => {
+      splitText.revert();
+    };
+  }, []);
+
+  return (
+    <main className="bg-primary -z-10 h-full">
+      <section className="h-screen">
+        <h2 className="font-oswald text-center text-6xl text-black">
           About Me
         </h2>
-      <div className='mt-10  mx-20 md:mx-2 lg:mx-20  sm:mx-0  sm:px-3 '>
-<div>
-    <p className={`font-open text-xl leading-[35px]`}>
-    Hey there!👋 My name is Chisom Blessing Ohanu. I'm a creative front-end developer
-     with a background in Mass Communication. With a passion for storytelling and design,
-      I now build beautiful, user-friendly websites and applications that engage and inspire.
-       I bring a unique blend of creativity and technical skills to the table, using frameworks
-        like Vue, React, Next.js, and Nuxt.js to bring ideas to life on the web.
-
-</p>
-    <p className={`font-open text-xl leading-[35px] mt-3`}>
-    Whether it's crafting seamless interfaces or diving into the latest code trends, 
-    I’m always excited to learn and grow. When I'm not coding, you can usually find me
-     exploring new restaurants, experimenting with flavors, or getting into the goofy 
-     side of life (laughter is the best bug fix, right?). I believe creativity knows no bounds,
-     and I bring that mindset to every project I take on. Let's create something awesome together!🚀</p>
-</div>
-      </div>
-     
+        <div className="mt-10 mx-20 md:mx-2 lg:mx-20 sm:mx-0 sm:px-3">
+          <div>
+            <p className="font-open text-xl leading-[35px] aboutmain text-wrap">
+              Hey there!👋 My name is Chisom Blessing Ohanu. I'm a creative
+              front-end developer with a background in Mass Communication. With
+              a passion for storytelling and design, I now build beautiful,
+              user-friendly websites and applications that engage and inspire. I
+              bring a unique blend of creativity and technical skills to the
+              table, using frameworks like Vue, React, Next.js, and Nuxt.js to
+              bring ideas to life on the web.
+            </p>
+            <p className="font-open text-xl leading-[35px] mt-3 aboutmain">
+              Whether it's crafting seamless interfaces or diving into the
+              latest code trends, I’m always excited to learn and grow. When I'm
+              not coding, you can usually find me exploring new restaurants,
+              experimenting with flavors, or getting into the goofy side of life
+              (laughter is the best bug fix, right?). I believe creativity knows
+              no bounds, and I bring that mindset to every project I take on.
+              Let's create something awesome together!🚀
+            </p>
+          </div>
+        </div>
       </section>
-    </main>    </> );
-}
- 
+    </main>
+  );
+};
+
 export default About;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,48 +1,37 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { ReactLenis, useLenis } from "lenis/react";
 import Lenis from "lenis";
-import Navbar from "@/components/navbar/page"; 
-import './globals.css';
-import {Oswald ,Open_Sans} from 'next/font/google'
-
-
+import gsap from "gsap";
+import Navbar from "@/components/navbar/page";
+import "./globals.css";
+import { Oswald, Open_Sans } from "next/font/google";
 
 const open = Open_Sans({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 const oswald = Oswald({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function RootLayout({ children }) {
+  const lenisRef = useRef();
+
   useEffect(() => {
-    const lenis = new Lenis({
-      smoothWheel: true,
-      smoothTouch: true,
-      lerp: 0.5, 
-    });
-
-   // <Link href="/maincv.pdf"  passHref>
-   // <a download="maincv.pdf">Download CV</a>
-   // </Link>
-
-    function animate(time) {
-      lenis.raf(time);
-      requestAnimationFrame(animate);
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
     }
 
-    requestAnimationFrame(animate);
+    gsap.ticker.add(update);
 
-    return () => lenis.destroy();
+    return () => gsap.ticker.remove(update);
   }, []);
 
   return (
     <html lang="en">
-     
       <body className={`${open.className} ${oswald.className} antialiased`}>
-        <ReactLenis>
+        <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
           <Navbar />
           {children}
         </ReactLenis>
