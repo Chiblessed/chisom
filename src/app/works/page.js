a6b4a176de6abfbe0afc7fd12a52a492dc255d44
Chiblessed <chizzyhanz56@gmail.com>
2025-10-13 01:14:48 -1100

    Project Updated

--- src/app/works/page.js
+++ src/app/works/page.js
@@ -8,6 +8,7 @@ import Image1 from "../../../public/Screenshot (35).png";
 import Image3 from "../../../public/beltelcake.png";
 import Image4 from "../../../public/afrika.png";
 import Image5 from "../../../public/image0 (3).jpeg";
+import Image7 from "../../../public/venture.png";
 import Link from "next/link";
 import Image from "next/image";
 import WorkButton from "@/components/workbtn/page";
@@ -191,7 +192,7 @@ const Work = () => {
                   evokes a sense of delight and indulgence, mirroring the
                   brand’s dedication to making every moment feel special.
                 </p>
-                <ul className="flex gap-3 sm:gap-1 sm:mb-3 font-open">
+                <ul className="flex sm:grid sm:grid-cols-3 gap-3 sm:gap-1 sm:mb-3 font-open">
                   <li className="bg-second text-primary text-sm px-3 py-2 rounded-sm cursor-pointer">
                     Next.js
                   </li>
@@ -203,6 +204,9 @@ const Work = () => {
                   </li>
                   <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
                     Context API
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    E-commerce
                   </li>
                   <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
                     Firebase
@@ -276,6 +280,63 @@ const Work = () => {
                 </WorkButton>
               </Link>
               <Link href="https://afri-tawny.vercel.app/">
+                <WorkButton>
+                  <p className="text-lg sm:text-base transition-colors duration-500 ease-linear z-50">
+                    Github
+                  </p>
+                </WorkButton>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-between sm:flex-col md:flex-col border-t border-t-second py-5">
+            <div className="flex items-start justify-center sm:flex-col gap-3">
+              <Image src={Image7} width={500} height={500} alt="project1" />
+              <div className="flex flex-col">
+                <h1 className="text-second font-oswald text-3xl uppercase">
+                  Venture Verse
+                </h1>
+                <p className="text-sm py-5 w-[70%] sm:w-full md:w-full font-open">
+                  This platform fuses innovation, creativity, and play through a
+                  sleek, immersive design that embodies Venture Verse’s spirit
+                  of digital exploration. Subtle gradients, fluid animations,
+                  and interactive elements draw users into a world where
+                  technology meets imagination. From minting NFTs to showcasing
+                  ventures and games, every detail feels intentional and
+                  futuristic — reflecting the platform’s mission to empower
+                  creators, gamers, and investors to own, build, and shape the
+                  next frontier of Web3.
+                </p>
+                <ul className="flex sm:grid sm:grid-cols-3 gap-3 sm:gap-1 sm:mb-3 font-open">
+                  <li className="bg-second text-primary text-sm px-3 py-2 rounded-sm cursor-pointer">
+                    Next.js
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    Tailwindcss
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    dAPP
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    web3.js
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    Solidity
+                  </li>
+                  <li className="bg-second text-primary text-sm px-3 py-2 cursor-pointer rounded-sm">
+                    GSAP
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex items-center justify-center gap-3 md:mt-3">
+              <Link href="https://selhono-interior.netlify.app/">
+                <WorkButton>
+                  <p className="text-lg sm:text-base transition-colors duration-500 ease-linear z-50">
+                    Live
+                  </p>
+                </WorkButton>
+              </Link>
+              <Link href="https://github.com/Chiblessed/selhono">
                 <WorkButton>
                   <p className="text-lg sm:text-base transition-colors duration-500 ease-linear z-50">
                     Github
